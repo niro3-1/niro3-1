f7a15291e5f9d7249d48a0687b3e1ca1116367be
niro3-1 <niro3-1@walla.co.il>
2026-05-17 15:19:58 +0300

    Fix onboarding flow progress loss

---
+++ onboarding_flow.js
@@ -0,0 +1,6 @@
+// Updated onboarding flow logic to preserve progress
+function onboardingFlow() {
+    let progress = getProgress();
+    // Logic to maintain progress
+    saveProgress(progress);
+}
